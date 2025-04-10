2b2d3c477c92a2236ceee69371d71a20dd9ece68
方忠朋 <992565438@qq.com>
2025-04-10 17:45:41 +0800

    feat(docs): 更新博主头像并优化首页内容
    - 更新博主头像为新的链接
    - 修改首页 heroText 为"不靠谱计划"
    - 更新 tagline 为"一个不靠谱的博客，致敬每一个爱学习的你。"
    - 移除 index.styl 中的多余空行

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -183,7 +183,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
 
     // 博主信息 (显示在首页侧边栏)
     blogger: {
-      avatar: 'https://jsd.cdn.zzko.cn/gh/xabcs/image_store/blog/20200103123203.jpg',
+      avatar: 'https://cdn.jsdelivr.net/gh/xabcs/picx-images-hosting@master/20250410/4087996879.60uffrhqgc.webp',
       name: '小龙龟',
       slogan: '前端界的小学生',
     },
